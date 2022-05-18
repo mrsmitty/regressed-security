5144b3c19c77e454c44c6b27af25415492ff456c
Paul Smithdale <8188140+mrsmitty@users.noreply.github.com>
2022-05-18 10:02:42 +0800

    remove readonly

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -2,7 +2,7 @@ namespace regressed_security.Data;
 
 public class WeatherForecastService
 {
-    private static readonly string[] Summaries = new[]
+    private static string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
